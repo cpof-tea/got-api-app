9482026c990b397a2bb0cebfd1a581df597e5ff6
Oleg Makarenko <oleg.makarenko@pm.me>
2022-03-29 00:03:31 +0400

    Update `App` component to render characters and houses

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router, useSearchParams } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const LocationRender = () => {
+	const [searchParams, ] = useSearchParams();
+
+	return (
+		<span>{ searchParams.toString('page') }</span>
+	);
+}
+
+const withRouter = (Component) => (
+	<Router>
+		{ Component }
+	</Router>
+);
+
+test('app render', async () => {
+	const { rerender } = render(<App />);
+
+	// Navigate to next page
+	const nextPageButton = await screen.findByRole('button', { name: /next page/i });
+
+	fireEvent.click(nextPageButton);
+
+	await screen.findByText(/page 2 of /i);
+	await screen.findByText(/previous page/i);
+
+	// Check if app respects location on rerender
+	rerender(<App />);
+
+	await screen.findByText(/previous page/i);
+
+	// Check location
+	rerender(withRouter(<LocationRender />));
+
+	await screen.findByText('page=2&pageSize=25');
 });
+
